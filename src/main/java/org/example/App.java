d597a3f7ff2dbdb42f4fd25fb6440c417fbb81dc
Egor Ruzanov <ruzanow35@gmail.com>
2025-03-01 21:18:20 +0400

    Добавлены возможности добавить, удалить задачи и вывести все задачи в консоль

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -13,47 +13,26 @@ public class App {
         TaskList task_list = new TaskList();
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Выберите действие (введите число):  \n(1) Создать новую задачу \n(2) Удалить задачу по индексу \n(3) Изменить задачу по индексу \n(4) Вывести все задачи\n");
+        System.out.println("Выберите действие (введите число):  \n(1) Создать новую задачу " +
+                "\n(2) Удалить задачу по индексу " +
+                "\n(3) Изменить задачу по индексу " +
+                "\n(4) Вывести все задачи\n");
         int choice = scanner.nextInt();
         scanner.close();
 
         switch (choice){
             case 1:
-                createTask(task_list, dateFormatter);
+                task_list.createTask(dateFormatter);
                 break;
-                // Дописать кейсы
+            case 2:
+                task_list.removeTask();
+                break;
+            case 3:
+                break;
+            case 4:
+                task_list.printAllTasks();
+                break;
         }
 
-    }
-
-
-    // Убрать в класс TaskList
-    public static void createTask(TaskList task_list, DateTimeFormatter dateFormatter) throws IncorrectTask{
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Введите заголовок задачи:  ");
-        String title = scanner.nextLine();
-
-        System.out.println("Введите описание задачи:  ");
-        String description = scanner.nextLine();
-
-        System.out.println("Введите приоритет (0-низкий, 1-средний, 2-срочно, 3-максимально срочно):  ");
-        int priority = Integer.parseInt(scanner.nextLine());
-
-        System.out.println("Введите дедлайн в формате (дд/мм/гггг):");
-        LocalDate date = LocalDate.parse(scanner.nextLine(), dateFormatter);
-
-        System.out.println("Введите исполнителя:  (Оставьте пустым, если исполнитель - вы)");
-        String worker = scanner.nextLine();
-
-        Task task;
-        if (worker == null){
-            task = new Task(title, description, priority, date);
-        } else {
-            task = new Task(title, description, priority, date, worker);
-        }
-        task_list.addTask(task);
-
-        scanner.close();
     }
 }
--- src/main/java/org/example/TaskList.java
+++ src/main/java/org/example/TaskList.java
@@ -1,24 +1,55 @@
 package org.example;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.LinkedList;
+import java.util.Scanner;
 
 public class TaskList {
-    private LinkedList<Task> tasks;
+    private LinkedList<Task> task_list;
 
     public TaskList() {
-        tasks = new LinkedList<>();
+        task_list = new LinkedList<>();
     }
 
     // Метод для добавления задачи
-    public void addTask(Task task) {
-        tasks.add(task);
+    public void createTask(DateTimeFormatter dateFormatter) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите заголовок задачи:  ");
+        String title = scanner.nextLine();
+
+        System.out.println("Введите описание задачи:  ");
+        String description = scanner.nextLine();
+
+        System.out.println("Введите приоритет (0-низкий, 1-средний, 2-срочно, 3-максимально срочно):  ");
+        int priority = Integer.parseInt(scanner.nextLine());
+
+        System.out.println("Введите дедлайн в формате (ДД/ММ/ГГГГ):");
+        LocalDate date = LocalDate.parse(scanner.nextLine(), dateFormatter);
+
+        System.out.println("Введите исполнителя:  (Оставьте пустым, если исполнитель - вы)");
+        String worker = scanner.nextLine();
+        scanner.close();
+
+        Task task;
+        if (worker == null){
+            task = new Task(title, description, priority, date);
+        } else {
+            task = new Task(title, description, priority, date, worker);
+        }
+        task_list.add(task);
     }
 
     // Метод для удаления задачи по индексу
-    public void removeTask(int index) {
-        if (index >= 0 && index < tasks.size()) {
-            tasks.remove(index);
+    public void removeTask() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите номер задачи, которую следует удалить, нумерация начинается с 1:  ");
+        int index = scanner.nextInt();
+        index++;
+        scanner.close();
+        if (index >= 0 && index < task_list.size()) {
+            task_list.remove(index);
         } else {
             System.out.println("Задача с таким индексом не найдена.");
         }
@@ -26,8 +57,8 @@ public class TaskList {
 
     // Метод для редактирования задачи по индексу
     public void editTask(int index, String title, String description, int priority, LocalDate deadline) {
-        if (index >= 0 && index < tasks.size()) {
-            Task task = tasks.get(index);
+        if (index >= 0 && index < task_list.size()) {
+            Task task = task_list.get(index);
             task.editTitle(title);
             task.editDescription(description);
             task.editPriority(priority);
@@ -39,7 +70,7 @@ public class TaskList {
 
     // Метод для вывода всех задач
     public void printAllTasks() {
-        for (Task task : tasks) {
+        for (Task task : task_list) {
             task.print();
             System.out.println("--------------------------------------------------");
         }
@@ -47,6 +78,6 @@ public class TaskList {
 
     // Метод для получения количества задач
     public int getTaskCount() {
-        return tasks.size();
+        return task_list.size();
     }
 }
